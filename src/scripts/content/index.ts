40a4eb83a75e61e5d1e490a4e42bd52d3d5054af
wanghuimin45 <wanghuimin45@jd.com>
2023-10-30 14:57:37 +0800

    feat: 树形路由算法改造

--- src/scripts/content/index.ts
+++ src/scripts/content/index.ts
@@ -18,11 +18,112 @@ printLine("Using the 'printLine' function from the Print Module");
  *     - micro-app-xxx且不是micro-app-head或者micro-app-body，可以多个，递归
  */
 
+ function filterMicroApps(dom: any) {
+  const microApps = dom;
+  const filteredMicroApps = [];
+  for (const microApp of microApps) {
+    const micro = microApp.tagName.toLowerCase();
+    if (micro.startsWith('micro-app')
+    && micro !== 'micro-app-head'
+    && micro !== 'micro-app-body') {
+      filteredMicroApps.push(microApp);
+    }
+  }
+  return filteredMicroApps;
+}
+
+function arraysAreEqual(arr1: string | any[], arr2: string | any[]) {
+  // 检查数组长度是否相同
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  // 遍历数组元素
+  for (let i = 0; i < arr1.length; i++) {
+    const item1 = arr1[i];
+    const item2 = arr2[i];
+
+    // 如果元素是数组，则递归比较
+    if (Array.isArray(item1) && Array.isArray(item2)) {
+      if (!arraysAreEqual(item1, item2)) {
+        return false;
+      }
+    }
+    // 如果元素是对象，则递归比较
+    else if (typeof item1 === 'object' && typeof item2 === 'object') {
+      if (!objectsAreEqual(item1, item2)) {
+        return false;
+      }
+    }
+    // 比较基本类型元素
+    else if (item1 !== item2) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function objectsAreEqual(obj1: { [x: string]: any; }, obj2: { [x: string]: any; }) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  // 检查对象键的数量是否相同
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  // 遍历对象键
+  for (let key of keys1) {
+    const val1 = obj1[key];
+    const val2 = obj2[key];
+
+    // 如果值是数组，则递归比较
+    if (Array.isArray(val1) && Array.isArray(val2)) {
+      if (!arraysAreEqual(val1, val2)) {
+        return false;
+      }
+    }
+    // 如果值是对象，则递归比较
+    else if (typeof val1 === 'object' && typeof val2 === 'object') {
+      if (!objectsAreEqual(val1, val2)) {
+        return false;
+      }
+    }
+    // 比较基本类型值
+    else if (val1 !== val2) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// 找出DOM元素数组中的最顶层元素
+function findTopLevelElements(elements: any) {
+  const topLevelElements = [];
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    let isTopLevel = true;
+    for (let j = 0; j < elements.length; j++) {
+      if (i !== j && elements[j].contains(element)) {
+        isTopLevel = false;
+        break;
+      }
+    }
+    if (isTopLevel) {
+      topLevelElements.push(element);
+    }
+  }
+  return topLevelElements;
+}
+
 // 第一步：获取最顶层父级
 const topFather = window.location.href;
 
 // 第二步：获取父级micro-app
-const topFather_1_el_maps = document.getElementsByTagName('micro-app');
+// const allMicroApp = filterMicroApps(document.querySelectorAll('*'));
+// const topFather_1_el_maps = findTopLevelElements(allMicroApp);
 // 核心-开始递归寻址子级
 const topInitEntry = {
   // currentLevel: 0,
@@ -41,11 +142,14 @@ const hasChild = (context: Element) => context.getElementsByTagName('micro-app-b
 // 工具-找出最近的子集的DOM
 function getChildByDomRegex(citem) {
   // 多层嵌套时，子集标签格式：micro-app-XXX,但是不能包含['micro-app-head',"micro-app-body"]【 https://micro-zoe.github.io/micro-app/docs.html#/zh-cn/nest 】
-  const child_regex = /^(micro-app-).+(?<!(head|body))$/;
+  // const child_regex = /^(micro-app-).+(?<!(head|body))$/;
   const itemChildrens = citem.querySelectorAll('*');
   const output = [];
-  for (let i of itemChildrens) {
-    if (child_regex.test(i.tagName.toLowerCase())) {
+  for (const i of itemChildrens) {
+    const micro = i.tagName.toLowerCase();
+    if (micro.startsWith('micro-app')
+    && micro !== 'micro-app-head'
+    && micro !== 'micro-app-body') {
       output.push(i);
     }
   }
@@ -61,12 +165,8 @@ const getChildMap = (childItem: Element, faLevel = '0') => {
         const item = getChildByDomRegex(fa_maps[index]);
         const key = `${faLevel}-${index}`;
         const curItem = {
-          // currentLevel: index,
           key,
-          // name: item.getAttribute('name'),
-          title: item.getAttribute('url'),
-          // tag: item.tagName.toLowerCase(),
-          // element: item,
+          title: `${item.getAttribute('name')}:${item.getAttribute('url')}`,
           children: [],
         };
         topMap[key] = curItem;
@@ -81,6 +181,7 @@ const getChildMap = (childItem: Element, faLevel = '0') => {
 
 // 初始化：进行一级查找
 const getMap = (faLevel = '0') => {
+  const topFather_1_el_maps = findTopLevelElements(microApps);
   if (topFather_1_el_maps && topFather_1_el_maps.length > 0) {
     for (let index = 0; index < topFather_1_el_maps.length; index++) {
       const item = topFather_1_el_maps[index];
@@ -89,7 +190,7 @@ const getMap = (faLevel = '0') => {
         // currentLevel: index,
         key,
         // name: item.getAttribute('name'),
-        title: item.getAttribute('url'),
+        title: item.getAttribute('name') + ':' + item.getAttribute('url'),
         // tag: item.tagName.toLowerCase(),
         // element: item,
         children: [],
@@ -100,26 +201,13 @@ const getMap = (faLevel = '0') => {
       if (item && (hasChild(item.getElementsByTagName('micro-app-body')[0]))) {
         getChildMap(item, key);
       }
+      console.log(topMap['0'], '=======111111')
     }
   }
 };
 
-function filterMicroApps() {
-  const microApps = document.querySelectorAll('*');
-  const filteredMicroApps = [];
-  for (const microApp of microApps) {
-    const micro = microApp.tagName.toLowerCase();
-    if (micro.startsWith('micro-app')
-    && micro !== 'micro-app-head'
-    && micro !== 'micro-app-body') {
-      filteredMicroApps.push(microApp);
-    }
-  }
-  return filteredMicroApps;
-}
-
 // init microApp
-let microApps = filterMicroApps();
+let microApps = filterMicroApps(document.querySelectorAll('*'));
 /*
  * create button
  * const button = document.createElement('button');
@@ -131,9 +219,9 @@ let microApps = filterMicroApps();
  */
 setTimeout(() => {
   chrome.runtime.sendMessage({
-    updateIcon: filterMicroApps(),
+    updateIcon: filterMicroApps(document.querySelectorAll('*')),
   });
-  microApps = filterMicroApps();
+  microApps = filterMicroApps(document.querySelectorAll('*'));
   // (microApp as HTMLElement).style.border = '2px solid red';
 
   // // setting button
@@ -157,7 +245,7 @@ setTimeout(() => {
 // get msg and setting button or microApp
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   // to retrieve elements again within the page
-  microApps = filterMicroApps();
+  microApps = filterMicroApps(document.querySelectorAll('*'));
   chrome.runtime.sendMessage({
     updateIcon: microApps,
   });
@@ -175,29 +263,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   });
   if (request.action === 'devtoolsMicroApp') {
   // start-天龙八部
+    console.log(88888, '得到的顶层树形数据结构为：：：：', topMap['0']);
     if (Array.isArray(topMap['0'].children) && topMap['0'].children.length === 0) {
       getMap();
     }
-    const microAppDevs = filterMicroApps();
-    const urlResult = [];
-    const nameResult = [];
-    // microAppDevs.forEach(obj => result.push(obj.attributes[obj.attributes.length].value));
-    for (let i = 0; i < microAppDevs.length; i++) {
-      for (let j = 0; j < microAppDevs[i].attributes.length; j++) {
-        if (microAppDevs[i].attributes[j].value.startsWith('http') || microAppDevs[i].attributes[j].value.startsWith('//')) {
-          urlResult.push(microAppDevs[i].attributes[j].value);
-          nameResult.push(microAppDevs[i].attributes[j - 1].value);
-        }
-      }
-    }
-    const urlResultString = urlResult.join('@');
-    const nameResultString = nameResult.join('@');
-    const result = `${urlResultString}*${nameResultString}`;
-    console.log(urlResult, nameResult, result, '-------123456iii123');
-    console.log('microAppDevs', microAppDevs);
-    console.log('microAppDevs[0].attributes[4]', microAppDevs[0].attributes[4]);
-    // sendResponse(result);
-    console.log(JSON.stringify(topMap['0']), '----JSON.stringify(topMap['0'])111')
+    console.log(999999, '得到的顶层树形数据结构为：：：：', topMap['0']);
     sendResponse(JSON.stringify(topMap['0']));
   }
 });
