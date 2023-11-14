483863fc98cf586b5bce1d377748379622b34c60
chenhong289 <chenhong289@jd.com>
2023-11-14 12:34:10 +0800

    fix: 添加默认边框颜色 修改恢复初始值样式

--- src/scripts/content/index.ts
+++ src/scripts/content/index.ts
@@ -68,26 +68,26 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     return;
   }
   const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+  const defaultColor = 'black';
+  const originalStyles = new Map(); // Initial style used to save elements
   microApps.forEach((microApp, index) => {
     if (microApp) {
       if (request.action === 'openView') {
-        (microApp as HTMLElement).style.border = `2px solid ${colors[index % colors.length]}`;
+        const color = index < colors.length ? colors[index] : defaultColor;
+        const originalStyle = microApp.getAttribute('style'); // get initial style
+        originalStyles.set(microApp, originalStyle); // save initial style
+        (microApp as HTMLElement).style.border = `2px solid ${color}`;
+        // fix some parent div setting display: contents cause border colors cannot be displayed
         (microApp as HTMLElement).style.display = 'block';
         // add transform is because some border colors cannot be displayed
         (microApp as HTMLElement).style.transformOrigin = 'center';
         (microApp as HTMLElement).style.transform = 'rotate(360deg)';
-        const originalDisplay = (microApp as HTMLElement).style.display;
-        // fix some parent div setting display: contents cause border colors cannot be displayed
-        (microApp as HTMLElement).style.display = 'block';
-        (microApp as HTMLElement).dataset.originalDisplay = originalDisplay;
       } else if (request.action === 'closeView') {
-        (microApp as HTMLElement).style.border = '0px';
-        (microApp as HTMLElement).style.padding = '0px';
-        (microApp as HTMLElement).style.transform = 'none';
-        const originalDisplay = (microApp as HTMLElement).dataset.originalDisplay;
-        if (originalDisplay) {
-          (microApp as HTMLElement).style.display = originalDisplay;
-          delete (microApp as HTMLElement).dataset.originalDisplay;
+        const originalStyle = originalStyles.get(microApp); // get save elements
+        if (originalStyle) {
+          microApp.setAttribute('style', originalStyle); // set save elements
+        } else {
+          microApp.removeAttribute('style'); // if there is no initial style, remove all inline styles
         }
       }
     }
