08184f656fcd3d1698cae6bf35eac18bfff4bc33
zhouran8 <zhouran@jd.com>
2023-11-22 16:44:45 +0800

    feat: 做子应用传递给父应用数据输出

--- src/pages/devtools/components/communicate/index.tsx
+++ src/pages/devtools/components/communicate/index.tsx
@@ -1,19 +1,20 @@
+/* eslint-disable etc/no-commented-out-code */
 import React from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+// import { CopyToClipboard } from 'react-copy-to-clipboard';
 import ReactJson from 'react-json-view';
 
 import styles from './index.module.less';
 
-interface CommunicateProps {}
+interface CommunicateProps { }
 interface CommunicateState {
   info: { [key: string]: string | number };
-  isClickGetAppButton: boolean;
+  // isClickGetAppButton: boolean;
 }
 
 class CommunicatePage extends React.PureComponent<CommunicateProps, CommunicateState> {
   public state: CommunicateState = {
     info: {},
-    isClickGetAppButton: false,
+    // isClickGetAppButton: true,
   };
 
   /**
@@ -25,75 +26,96 @@ class CommunicatePage extends React.PureComponent<CommunicateProps, CommunicateS
       (res: string) => {
         if (res) {
           this.setState({ info: JSON.parse(JSON.stringify(res)) });
+        } else {
+          this.setState({
+            info: {}
+          })
         }
       },
     );
-    this.setState({ isClickGetAppButton: true });
+    // this.setState({ isClickGetAppButton: true });
+  }
+
+  private getMicroAppSendData() {
+    const evalLabel = `JSON.stringify(function () {
+      if (document.querySelector("micro-app").getSendData){
+          console.log('已设置过');
+      } else {
+          document.querySelector("micro-app").addEventListener('datachange', function (e) {
+              document.querySelector("micro-app").getSendData = e.detail.data;
+              document.querySelector("micro-app").onDataChange;
+              // alert('子应用发送的信息:' + JSON.stringify(e.detail.data));
+          })
+      }
+      return document.querySelector("micro-app").getSendData;
+  }())`;
+    chrome.devtools.inspectedWindow.eval(
+      evalLabel,
+      (res: string) => {
+        if (res) {
+          this.setState({ info: JSON.parse(res) });
+        } else {
+          this.setState({
+            info: {}
+          })
+        }
+      },
+    );
   }
 
   public render() {
     return (
-      <div style={{ display: 'flex' }}>
-        <div className={styles.container}>
+      <div className={styles.container}>
+        <div className={styles.leftBlock}>
           <button
-            className={styles.btn}
+            className={styles.oneBtn}
             type="button"
             onClick={() => {
               this.getRenderData();
             }}
           >
-            获取父应用数据
-
+            获取父应用传递给子应用数据
           </button>
-          <div
-            className={styles['app-link']}
+          <button
+            className={styles.oneBtn}
+            type="button"
+            onClick={() => {
+              this.getMicroAppSendData();
+            }}
           >
-            <button
-              className={styles.btn}
-              type="button"
-              onClick={() => {
-                chrome.tabs.create({
-                  url: 'simulation.html',
-                });
-              }}
-            >
-              子应用开发环境模拟
-            </button>
-          </div>
+            获取子应用传递给父应用数据
+          </button>
+          <button
+            className={styles.oneBtn}
+            type="button"
+            onClick={() => {
+              chrome.tabs.create({
+                url: 'simulation.html',
+              });
+            }}
+          >
+            子应用开发环境模拟
+          </button>
         </div>
-        {
-          this.state.isClickGetAppButton && (
-          <div
-            style={{ backgroundColor: '#FFF', width: 800, height: 290, overflow: 'hidden', marginLeft: 20, marginTop: 20 }}
+        <div
+          className={styles.rightBlock}
+        >
+          <ReactJson
+            style={{ overflowY: 'scroll', overflowX: 'hidden', height: 290 }}
+            src={this.state.info}
+            name={false}
+          />
+        </div>
+        {/* <CopyToClipboard
+          text={JSON.stringify(this.state.info)}
+        >
+          <button
+            className={styles.oneBtn}
+            type="button"
           >
-            <ReactJson
-              style={{ overflowY: 'scroll', overflowX: 'hidden', height: 290 }}
-              src={this.state.info}
-              name={false}
-            />
-          </div>
-          )
-        }
-        {
-          this.state.isClickGetAppButton && (
-            <CopyToClipboard
-              text={JSON.stringify(this.state.info)}
-            >
-              <button
-                style={{ backgroundColor: '#00bfff',
-                  width: 80,
-                  height: 30,
-                  marginLeft: 20,
-                  marginTop: 20,
-                  borderRadius: 5,
-                  borderColor: ' #00bfff' }}
-                type="button"
-              >
-                复制
-              </button>
-            </CopyToClipboard>
-          )
-        }
+            复制
+          </button>
+        </CopyToClipboard> */}
       </div>
     );
   }
