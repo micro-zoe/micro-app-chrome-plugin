d55a298e7ae651205611a492fc4a5b2a2a80287b
zhouran8 <zhouran@jd.com>
2023-12-01 10:15:47 +0800

    fix: 将官网链接域名替换成Github的

--- src/pages/devtools/components/micro-app-env/index.tsx
+++ src/pages/devtools/components/micro-app-env/index.tsx
@@ -26,7 +26,7 @@ const MicroAppEnv: React.FC<MicroAppEnvProps> = (props) => {
   }
 
   const showDetailIcon = (link: string) => {
-    return <a href={link || 'https://zeroing.jd.com/micro-app/docs.html#/zh-cn/env'} className={styles.detailIcon} target='blank'><svg viewBox="64 64 896 896" focusable="false" data-icon="info-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path><path d="M464 336a48 48 0 1096 0 48 48 0 10-96 0zm72 112h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V456c0-4.4-3.6-8-8-8z"></path></svg></a>
+    return <a href={link || 'https://micro-zoe.github.io/micro-app/docs.html#/zh-cn/env'} className={styles.detailIcon} target='blank'><svg viewBox="64 64 896 896" focusable="false" data-icon="info-circle" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path><path d="M464 336a48 48 0 1096 0 48 48 0 10-96 0zm72 112h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8h48c4.4 0 8-3.6 8-8V456c0-4.4-3.6-8-8-8z"></path></svg></a>
   }
 
   return (
--- src/pages/devtools/config/index.ts
+++ src/pages/devtools/config/index.ts
@@ -8,22 +8,22 @@ export const HEADER_TAB_LIST = [
 export const MICRO_APP_ENV_INFO = {
   __MICRO_APP_ENVIRONMENT__: {
     describe: '判断应用是否在微前端环境中',
-    url: 'https://zeroing.jd.com/micro-app/docs.html#/zh-cn/env?id=__micro_app_environment__',
+    url: 'https://micro-zoe.github.io/micro-app/docs.html#/zh-cn/env?id=__micro_app_environment__',
   },
   __MICRO_APP_NAME__: {
     describe: '应用名称',
-    url: 'https://zeroing.jd.com/micro-app/docs.html#/zh-cn/env?id=__micro_app_name__',
+    url: 'https://micro-zoe.github.io/micro-app/docs.html#/zh-cn/env?id=__micro_app_name__',
   },
   __MICRO_APP_PUBLIC_PATH__: {
     describe: '子应用的静态资源前缀',
-    url: 'https://zeroing.jd.com/micro-app/docs.html#/zh-cn/env?id=__micro_app_public_path__',
+    url: 'https://micro-zoe.github.io/micro-app/docs.html#/zh-cn/env?id=__micro_app_public_path__',
   },
   __MICRO_APP_BASE_URL__: {
     describe: '子应用的基础网址',
-    url: 'https://zeroing.jd.com/micro-app/docs.html#/zh-cn/env?id=__micro_app_base_route__',
+    url: 'https://micro-zoe.github.io/micro-app/docs.html#/zh-cn/env?id=__micro_app_base_route__',
   },
   __MICRO_APP_BASE_ROUTE__: {
     describe: '子应用的基础路由',
-    url: 'https://zeroing.jd.com/micro-app/docs.html#/zh-cn/env?id=__micro_app_base_route__',
+    url: 'https://micro-zoe.github.io/micro-app/docs.html#/zh-cn/env?id=__micro_app_base_route__',
   },
 };
